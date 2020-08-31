29f51effe1cd7eb57f9c04104570f74340ef8faf
Diego Rios <d.riosrojas007@gmail.com>
2020-08-31 14:23:25 -0400

    cambios hechos

--- routes/api/sucursales.js
+++ routes/api/sucursales.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
 const { Sucursal } = require("../../db");
 
-router.get("/", async (req, res) => {
-  const sucursales = await Sucursal.findAll();
-  res.json({
-    success: true,
-    data: sucursales,
-  });
+router.get("/cargarSucursales", async(req, res) => {
+    const sucursales = await Sucursal.findAll();
+    res.json({
+        success: true,
+        data: sucursales,
+    });
 });
 
 module.exports = router;
--- routes/api/vehiculos.js
+++ routes/api/vehiculos.js
@@ -2,46 +2,57 @@ const router = require("express").Router();
 
 const { Vehiculo, Sucursal } = require("../../db");
 
-router.get("/", async (req, res) => {
-  const vehiculos = await Vehiculo.findAll({
-    include: Sucursal,
-  });
-  res.json({
-    success: true,
-    data: vehiculos,
-  });
+router.get("/cargarVehiculos", async(req, res) => {
+    const vehiculos = await Vehiculo.findAll({
+        include: Sucursal,
+    });
+    res.json({
+        success: true,
+        data: vehiculos,
+    });
 });
 
-router.post("/", async (req, res) => {
-  const vehiculo = await Vehiculo.create(req.body);
-  res.json({
-    success: true,
-    msg: " Vehiculo creado exitosamente",
-    data: vehiculo,
-  });
+router.get("/cargarUnVehiculo/:vehiculoId", async(req, res) => {
+    const vehiculos = await Vehiculo.findAll({
+        include: Sucursal,
+        where: { patente_vehiculo: req.params.vehiculoId },
+    });
+    res.json({
+        success: true,
+        data: vehiculos,
+    });
 });
 
-router.put("/:vehiculoId", async (req, res) => {
-  await Vehiculo.update(req.body, {
-    where: { patente_vehiculo: req.params.vehiculoId },
-  });
-
-  res.json({
-    success: true,
-    msg: "Vehiculo modificado exitosamente",
-    data: req.body,
-  });
+router.post("/registrarVehiculo", async(req, res) => {
+    const vehiculo = await Vehiculo.create(req.body);
+    res.json({
+        success: true,
+        msg: " Vehiculo creado exitosamente",
+        data: vehiculo,
+    });
 });
 
-router.delete("/:vehiculoId", async (req, res) => {
-  await Vehiculo.destroy({
-    where: { patente_vehiculo: req.params.vehiculoId },
-  });
-  res.json({
-    success: true,
-    msg: " Vehiculo borrado exitosamente",
-    data: req.params.vehiculoId,
-  });
+router.put("/editarVehiculo/:vehiculoId", async(req, res) => {
+    await Vehiculo.update(req.body, {
+        where: { patente_vehiculo: req.params.vehiculoId },
+    });
+
+    res.json({
+        success: true,
+        msg: "Vehiculo modificado exitosamente",
+        data: req.body,
+    });
+});
+
+router.delete("/eliminarVehiculo/:vehiculoId", async(req, res) => {
+    await Vehiculo.destroy({
+        where: { patente_vehiculo: req.params.vehiculoId },
+    });
+    res.json({
+        success: true,
+        msg: " Vehiculo borrado exitosamente",
+        data: req.params.vehiculoId,
+    });
 });
 
 module.exports = router;
