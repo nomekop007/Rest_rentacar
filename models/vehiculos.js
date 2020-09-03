ebdc976f65b30d7d0fa5cff95d08180a7fe0342e
Diego Rios <d.riosrojas007@gmail.com>
2020-09-03 12:20:06 -0400

    agregacion de atributos a la tabla vehiculos

--- models/vehiculos.js
+++ models/vehiculos.js
@@ -1,16 +1,20 @@
 module.exports = (sequelize, type) => {
-  return sequelize.define("vehiculos", {
-    patente_vehiculo: {
-      type: type.STRING(11),
-      primaryKey: true,
-    },
-    modelo_vehiculo: type.STRING,
-    tipo_vehiculo: type.STRING,
-    color_vehiculo: type.STRING,
-    precio_vehiculo: type.INTEGER,
-    propietario_vehiculo: type.STRING,
-    fechaCompra_vehiculo: type.DATE,
-    año_vehiculo: type.INTEGER,
-    foto_vehiculo: type.STRING,
-  });
+    return sequelize.define("vehiculos", {
+        patente_vehiculo: {
+            type: type.STRING(11),
+            primaryKey: true,
+        },
+        estado_vehiculo: type.STRING,
+        chasis_vehiculo: type.STRING,
+        numeroMotor_vehiculo: type.STRING,
+        marca_vehiculo: type.STRING,
+        modelo_vehiculo: type.STRING,
+        tipo_vehiculo: type.STRING,
+        color_vehiculo: type.STRING,
+        precio_vehiculo: type.INTEGER,
+        propietario_vehiculo: type.STRING,
+        fechaCompra_vehiculo: type.DATE,
+        año_vehiculo: type.INTEGER,
+        foto_vehiculo: type.STRING,
+    });
 };
