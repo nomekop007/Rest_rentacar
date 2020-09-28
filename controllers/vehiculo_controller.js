519da9c493aa165dc60733fd230f4f9b2a34d14f
diego <d.riosrojas007@gmail.com>
2020-09-27 21:52:01 -0300

    cambiar guardados

--- controllers/vehiculo_controller.js
+++ controllers/vehiculo_controller.js
@@ -3,139 +3,143 @@ const fs = require("fs");
 const path = require("path");
 
 class VehiculoController {
-    async getVehiculos(req, res) {
-        const vehiculos = await Vehiculo.findAll({
-            include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
-            attributes: [
-                "patente_vehiculo",
-                "modelo_vehiculo",
-                "año_vehiculo",
-                "tipo_vehiculo",
-                "transmision_vehiculo",
-                "estado_vehiculo",
-            ],
-        });
-        res.json({
-            success: true,
-            data: vehiculos,
-        });
+  async getVehiculos(req, res) {
+    const vehiculos = await Vehiculo.findAll({
+      include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
+      attributes: [
+        "patente_vehiculo",
+        "modelo_vehiculo",
+        "año_vehiculo",
+        "tipo_vehiculo",
+        "transmision_vehiculo",
+        "estado_vehiculo",
+      ],
+    });
+    res.json({
+      success: true,
+      data: vehiculos,
+    });
+  }
+
+  async findVehiculo(req, res) {
+    const vehiculo = await Vehiculo.findOne({
+      where: { patente_vehiculo: req.params.id },
+    });
+    if (vehiculo) {
+      res.json({
+        success: true,
+        data: vehiculo,
+      });
+    } else {
+      res.json({
+        success: false,
+        msg: "sin datos",
+      });
+    }
+  }
+
+  async createVehiculo(req, res) {
+    const response = req.body;
+
+    const [v, created] = await Vehiculo.findOrCreate({
+      where: { patente_vehiculo: response.patente_vehiculo },
+      defaults: response,
+    });
+    if (created) {
+      const vehiculo = await Vehiculo.findOne({
+        include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
+        where: { patente_vehiculo: v.patente_vehiculo },
+        attributes: [
+          "patente_vehiculo",
+          "modelo_vehiculo",
+          "año_vehiculo",
+          "tipo_vehiculo",
+          "transmision_vehiculo",
+          "estado_vehiculo",
+        ],
+      });
+
+      res.json({
+        success: true,
+        msg: " Vehiculo creado exitosamente",
+        data: vehiculo,
+      });
+    } else {
+      res.json({
+        success: false,
+        msg: " Vehiculo ya existe",
+      });
+    }
+  }
+
+  async updateVehiculo(req, res) {
+    await Vehiculo.update(req.body, {
+      where: { patente_vehiculo: req.params.id },
+    });
+
+    const vehiculo = await Vehiculo.findOne({
+      include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
+      where: { patente_vehiculo: req.params.id },
+      attributes: [
+        "patente_vehiculo",
+        "modelo_vehiculo",
+        "año_vehiculo",
+        "tipo_vehiculo",
+        "transmision_vehiculo",
+        "estado_vehiculo",
+      ],
+    });
+
+    res.json({
+      success: true,
+      msg: "Vehiculo modificado exitosamente",
+      data: vehiculo,
+    });
+  }
+
+  async deleteVehiculo(req, res) {
+    await Vehiculo.destroy({
+      where: { patente_vehiculo: req.params.id },
+    });
+    res.json({
+      success: true,
+      msg: " Vehiculo borrado exitosamente",
+      data: req.params.id,
+    });
+  }
+
+  async uploadImageVehiculo(req, res) {
+    const vehiculo = await Vehiculo.findOne({
+      where: { patente_vehiculo: req.params.id },
+    });
+
+    // se pregunta si el vehiculo  tiene image asignada
+    if (vehiculo.foto_vehiculo) {
+      //se borra la actual para remplazarla por la nueva
+      borrarImagenDeStorage(vehiculo.foto_vehiculo);
     }
 
-    async findVehiculo(req, res) {
-        const vehiculo = await Vehiculo.findOne({
-            where: { patente_vehiculo: req.params.id },
-        });
-        if (vehiculo) {
-            res.json({
-                success: true,
-                data: vehiculo,
-            });
-        } else {
-            res.json({
-                success: false,
-                msg: "sin datos",
-            });
-        }
-    }
+    await Vehiculo.update(
+      { foto_vehiculo: req.file.filename },
+      {
+        where: { patente_vehiculo: req.params.id },
+      }
+    );
 
-    async createVehiculo(req, res) {
-        const response = req.body;
-
-        const [v, created] = await Vehiculo.findOrCreate({
-            where: { patente_vehiculo: response.patente_vehiculo },
-            defaults: response,
-        });
-        if (created) {
-            const vehiculo = await Vehiculo.findOne({
-                include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
-                where: { patente_vehiculo: v.patente_vehiculo },
-                attributes: [
-                    "patente_vehiculo",
-                    "modelo_vehiculo",
-                    "año_vehiculo",
-                    "tipo_vehiculo",
-                    "transmision_vehiculo",
-                    "estado_vehiculo",
-                ],
-            });
-
-            res.json({
-                success: true,
-                msg: " Vehiculo creado exitosamente",
-                data: vehiculo,
-            });
-        } else {
-            res.json({
-                success: false,
-                msg: " Vehiculo ya existe",
-            });
-        }
-    }
-
-    async updateVehiculo(req, res) {
-        await Vehiculo.update(req.body, {
-            where: { patente_vehiculo: req.params.id },
-        });
-
-        const vehiculo = await Vehiculo.findOne({
-            include: [{ model: Sucursal, attributes: ["nombre_sucursal"] }],
-            where: { patente_vehiculo: req.params.id },
-            attributes: [
-                "patente_vehiculo",
-                "modelo_vehiculo",
-                "año_vehiculo",
-                "tipo_vehiculo",
-                "transmision_vehiculo",
-                "estado_vehiculo",
-            ],
-        });
-
-        res.json({
-            success: true,
-            msg: "Vehiculo modificado exitosamente",
-            data: vehiculo,
-        });
-    }
-
-    async deleteVehiculo(req, res) {
-        await Vehiculo.destroy({
-            where: { patente_vehiculo: req.params.id },
-        });
-        res.json({
-            success: true,
-            msg: " Vehiculo borrado exitosamente",
-            data: req.params.id,
-        });
-    }
-
-    async uploadImageVehiculo(req, res) {
-        const vehiculo = await Vehiculo.findOne({
-            where: { patente_vehiculo: req.params.id },
-        });
-
-        // se pregunta si el vehiculo  tiene image asignada
-        if (vehiculo.foto_vehiculo) {
-            borrarImagenDeStorage(vehiculo.foto_vehiculo);
-        }
-
-        await Vehiculo.update({ foto_vehiculo: req.file.filename }, {
-            where: { patente_vehiculo: req.params.id },
-        });
-
-        res.json({
-            success: true,
-            msg: " imagen guardada",
-        });
-    }
+    res.json({
+      success: true,
+      msg: " imagen guardada",
+    });
+  }
 }
 
 function borrarImagenDeStorage(name) {
-    try {
-        fs.unlinkSync(path.join(__dirname, "../uploads/fotosVehiculos/" + name));
-        return true;
-    } catch (err) {
-        return false;
-    }
+  try {
+    fs.unlinkSync(path.join(__dirname, "../uploads/fotosVehiculos/" + name));
+    return true;
+  } catch (err) {
+    return console.log(err);
+  }
 }
 
 module.exports = VehiculoController;
