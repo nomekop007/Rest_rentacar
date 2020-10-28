9b1d8b7b528c7c056f43bc0e45a138274413a7d8
Diego Rios <d.riosrojas007@gmail.com>
2020-10-28 15:52:20 -0300

    correciones

--- routes/defaultValues.js
+++ routes/defaultValues.js
@@ -156,7 +156,7 @@ router.get("/cargarVehiculos", async(req, res) => {
                 fechaCompra_vehiculo: vehiculos[i].fechaCompra,
                 estado_vehiculo: "DISPONIBLE",
                 kilometraje_vehiculo: 0,
-                id_sucursal: getRandomInt(1, 4),
+                id_sucursal: 1,
                 userAt: "default",
             };
             console.log(vehiculo);
