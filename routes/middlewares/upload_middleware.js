219a72d5b6f42c6ed1011975c507371724eaf29e
diego <d.riosrojas007@gmail.com>
2020-10-31 10:29:59 -0300

    correcion errores

--- routes/middlewares/upload_middleware.js
+++ routes/middlewares/upload_middleware.js
@@ -3,63 +3,54 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "../../uploads/fotosVehiculos"),
-    filename: (req, file, cb) => {
-        cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
-    },
+  destination: path.join(__dirname, "../../uploads/fotosVehiculos"),
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
+  },
 });
 
 const subirImageVehiculo = multer({
-    storage: storage,
-    dest: path.join(__dirname, "../../uploads/fotosVehiculos"),
-    limits: { fieldSize: 20000000 },
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/;
-        const mimetype = fileTypes.test(file.mimetype);
-        const extname = fileTypes.test(path.extname(file.originalname));
-        if (mimetype && extname) {
-            return cb(null, true);
-        }
-        cb("Error: Archivo debe ser una imagen valida");
-    },
+  storage: storage,
+  dest: path.join(__dirname, "../../uploads/fotosVehiculos"),
+  limits: { fieldSize: 20000000 },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const mimetype = fileTypes.test(file.mimetype);
+    const extname = fileTypes.test(path.extname(file.originalname));
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb("Error: Archivo debe ser una imagen valida");
+  },
 }).single("foto_vehiculo");
 
 const storage2 = multer.diskStorage({
-    destination: path.join(
-        __dirname,
-        "../../uploads/documentos/requisitosArriendo"
-    ),
-    filename: (req, file, cb) => {
-        cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
-    },
+  destination: path.join(
+    __dirname,
+    "../../uploads/documentos/requisitosArriendo"
+  ),
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
+  },
 });
 
 const subirDocumentoRequisitosArriendo = multer({
-    storage: storage2,
-    dest: path.join(__dirname, "../../uploads/documentos/requisitosArriendo"),
-    limits: { fieldSize: 10000000 },
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif|pdf/;
-        const mimetype = fileTypes.test(file.mimetype);
-        const extname = fileTypes.test(path.extname(file.originalname));
-        if (mimetype && extname) {
-            return cb(null, true);
-        }
-        cb("Error: Archivo debe ser una imagen valida");
-    },
+  storage: storage2,
+  dest: path.join(__dirname, "../../uploads/documentos/requisitosArriendo"),
+  limits: { fieldSize: 20000000 },
 }).fields([
-    { name: "fotoCarnetFrontal", maxCount: 1 },
-    { name: "fotoCarnetTrasera", maxCount: 1 },
-    { name: "fotoTarjetaFrontal", maxCount: 1 },
-    { name: "fotoTarjetaTrasera", maxCount: 1 },
-    { name: "fotoLicencia", maxCount: 1 },
-    { name: "fotoCheque", maxCount: 1 },
-    { name: "fotoComprobante", maxCount: 1 },
-    { name: "fotoCartaRemplazo", maxCount: 1 },
-    { name: "fotoBoletaEfectivo", maxCount: 1 },
+  { name: "fotoCarnetFrontal", maxCount: 1 },
+  { name: "fotoCarnetTrasera", maxCount: 1 },
+  { name: "fotoTarjetaFrontal", maxCount: 1 },
+  { name: "fotoTarjetaTrasera", maxCount: 1 },
+  { name: "fotoLicencia", maxCount: 1 },
+  { name: "fotoCheque", maxCount: 1 },
+  { name: "fotoComprobante", maxCount: 1 },
+  { name: "fotoCartaRemplazo", maxCount: 1 },
+  { name: "fotoBoletaEfectivo", maxCount: 1 },
 ]);
 
 module.exports = {
-    subirImageVehiculo: subirImageVehiculo,
-    subirDocumentoRequisitosArriendo: subirDocumentoRequisitosArriendo,
+  subirImageVehiculo: subirImageVehiculo,
+  subirDocumentoRequisitosArriendo: subirDocumentoRequisitosArriendo,
 };
