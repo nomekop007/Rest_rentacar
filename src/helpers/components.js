f4bfcc9b73080b3ae5e3679b0a93b9715f15cd89
diego rios rojas <d.riosrojas007@gmail.com>
2021-02-18 16:00:33 -0300

    cambio de prueba

--- src/helpers/components.js
+++ src/helpers/components.js
@@ -8,7 +8,7 @@ const pdfFonts = require("pdfmake/build/vfs_fonts.js");
 const nodemailer = require("nodemailer");
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-//cambio hecho
+//cambio hecho 2
 const sendError = (error, req, res) => {
     console.log(error);
     logErrorRegister(req, error);
