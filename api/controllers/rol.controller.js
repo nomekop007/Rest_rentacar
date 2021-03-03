83fa57408f26b8cbd675a937317050a5b0d06977
diego rios rojas <d.riosrojas007@gmail.com>
2021-03-03 09:34:53 -0300

    rol.controller -> rol.buisness okey

--- api/controllers/rol.controller.js
+++ api/controllers/rol.controller.js
@@ -1,17 +1,14 @@
 class RolController {
 
-    constructor({ RolService, RolRepository, sendError }) {
+    constructor({ RolService, sendError }) {
         this.sendError = sendError;
         this._rolService = RolService;
-
-        //mover
-        this._serviceRol = RolRepository;
     }
 
 
     async getRoles(req, res) {
         try {
-            const roles = await this._serviceRol.getFindAll();
+            const roles = await this._rolService.getRoles();
             res.json({ success: true, data: roles, });
         } catch (error) {
             this.sendError(error, req, res);
@@ -21,8 +18,9 @@ class RolController {
 
     async createRol(req, res) {
         try {
-            const rol = await this._serviceRol.postCreate(req.body);
-            res.json({ success: true, data: rol })
+            const rol = req.body;
+            const rolRepo = await this._rolService.createRol(rol);
+            res.json({ success: true, data: rolRepo })
         } catch (error) {
             this.sendError(error, req, res);
         }
--- contexts/roles/domain/rol.business.js
+++ contexts/roles/domain/rol.business.js
@@ -1,5 +1,18 @@
 class RolBusiness {
 
+    constructor({ RolRepository }) {
+        this._rolRepository = RolRepository;
+    }
+
+    async getRoles() {
+        const roles = await this._rolRepository.getFindAll();
+        return roles;
+    }
+
+    async createRol(rol) {
+        const rolRepo = await this._rolRepository.postCreate(rol);
+        return rolRepo;
+    }
 }
 
 module.exports = RolBusiness;
